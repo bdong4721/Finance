e565063ca6cec0e45353874284cc3d9e16ab7e99
Jay Oh <47611901+bdong4721@users.noreply.github.com>
2022-02-21 10:16:46 +0900

    Update 추출.py
    Stock data extraction from text file

--- 추출.py
+++ 추출.py
@@ -1,2 +1,25 @@
+import pandas as pd
+import datetime
+import time
+import matplotlib.pyplot as plt
+
+
+
+
 df = pd.read_fwf('NDX_1min.txt', sep=" ", header =None, names = ['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume']
 print(df[687733:982719])
+""""
+with open('NDX_1min.txt') as f:
+    lines = f.readline()
+
+stock = pd.DataFrame()
+
+for stock in lines:
+    df.columns = ['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    
+    if stock ==0:
+        None
+    else:
+        stock_f = stock.append(lines, sort=Fale)
+    print(stock_f)    
+"""
